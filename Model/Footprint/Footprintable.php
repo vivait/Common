d49b75b4c917102525faa33860ba655260fc989e
Lewis Wright <lewis@vivait.co.uk>
2014-04-28 11:01:09 +0100

    Removed getEntityName from footprintable since that's part of the EntityEvent.

--- Model/Footprint/Footprintable.php
+++ Model/Footprint/Footprintable.php
@@ -7,6 +7,5 @@ namespace Vivait\Common\Model\Footprint;
 interface Footprintable {
 	public function getFootprintType();
 	public function getFootprintEntity();
-	public function getEntityName();
 	public function getUser();
 }
