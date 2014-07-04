f6630567c94e36276ff978ca81ac5eb0bda1ec8f
Lewis Wright <lewis@vivait.co.uk>
2014-07-04 14:48:15 +0100

    Added loader service to common

---
+++ Container/Service/LoaderService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Vivait\Common\Container\Service;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Vivait\Common\Container\ServiceEntity;
+
+class LoaderService
+{
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    /**
+     * @param ServiceEntity[] $collection
+     * @return mixed
+     */
+    public function loadServices($collection)
+    {
+        $services = [];
+
+        foreach ($collection as $entity) {
+            $services[] = $this->loadService($entity);
+        }
+
+        return $services;
+    }
+
+    public function loadService(ServiceEntity $entity) {
+        return $entity->loadService($this->container);
+    }
+}
---
+++ Container/ServiceEntity.php
@@ -0,0 +1,13 @@
+<?php
+namespace Vivait\Common\Container;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+interface ServiceEntity
+{
+    /**
+     * @param ContainerInterface $container
+     * @return object
+     */
+    public function loadService(ContainerInterface $container);
+}
