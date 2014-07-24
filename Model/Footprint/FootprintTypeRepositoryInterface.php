6a7f91925909bf571dd4b2b5c19ae5cc6efb7650
Lewis Wright <lewis@vivait.co.uk>
2014-07-24 18:20:51 +0100

    Moved footprint types out of Bravo and fixed numerous bugs and cleanups.

--- Model/Footprint/FootprintTypeRepositoryInterface.php
+++ Model/Footprint/FootprintTypeRepositoryInterface.php
@@ -2,23 +2,24 @@
 namespace Vivait\Common\Model\Footprint;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Query\Expr\Composite;
+use Doctrine\ORM\QueryBuilder;
 use Viva\BravoBundle\Entity\Customer;
 
-interface FootprintTypeRepositoryInterface {
-	/**
-	 * @param integer $id
-	 * @throws \InvalidArgumentException
-	 * @return Customer
-	 */
-	public function loadForeignEntity($id);
-
-	public function getByForeignId($entity_id);
+interface FootprintTypeRepositoryInterface
+{
+    /**
+     * @param integer $id
+     * @throws \InvalidArgumentException
+     * @return Customer
+     */
+    public function loadForeignEntity($id);
 
     /**
-     * @param $entity_ids
-     * @param $since
-     * @param UserInterface $user
-     * @return ArrayCollection|FootprintTypeInterface[]
+     * @param array        $foreignIds    A nested array containing a map of footprint types and their foreign ids: footprint_type => [foreign_ids]
+     * @return array
      */
-	public function getByForeignIds($entity_ids, $since = null, UserInterface $user = null);
+    public function getByForeignIds(
+      array $foreignIds
+    );
 }
